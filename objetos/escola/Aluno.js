38ee25467237d0ac9cd217b5a7d9f17476843fcc
Raphael Scheidegger G. <raphaelscheideggerguedes0406@gmail.com>
2025-07-23 11:07:47 -0300

    aula encapsulamento

--- objetos/escola/Aluno.js
+++ objetos/escola/Aluno.js
@@ -1,7 +1,29 @@
+/*
+-------------------
+Nome da classes
+-------------------
+-matrícula: Number
+-curso: String
+-escola: String
+-------------------
++boolean setMatricula(matricula: Number)
++Number getMatricula() -> Não faz sentido retornar void na função get
+
++boolean setCurso(curso: String)
++String getCurso() -> Não faz sentido retornar void na função get
+
++boolean setEscola(escola : String)
++String getEscola() -> Não faz sentido retornar void na função get
+*/
+
+
 class Aluno 
 {
     #matricula; // atribui matricula como atributo de aluno
+    #curso; // atribui matricula como atributo de aluno
+    #escola; // atribui matricula como atributo de aluno
 
+    //Métodos matricula
     setMatricula(matricula)
     {
         this.#matricula = matricula;
@@ -10,6 +32,30 @@ class Aluno
     getMatricula()
     {
         return this.#matricula;
+    }
+
+
+    //Método curso
+    setCurso(curso)
+    {
+        this.#curso = curso;
+    }
+
+    getCurso()
+    {
+        return this.#curso;
+    }
+
+    
+    //Método escola
+    setEscola(escola)
+    {
+        this.#escola = escola;
+    }
+
+    getEscola()
+    {
+        return this.#escola;
     }
 }
 
