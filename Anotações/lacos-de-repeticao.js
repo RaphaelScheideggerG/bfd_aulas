d36481d1ba07c772be9d14d9fb84a9130e1b4cc6
Raphael Scheidegger G. <raphaelscheideggerguedes0406@gmail.com>
2025-08-20 10:28:12 -0300

    aula sobre loops

--- Anotações/lacos-de-repeticao.js
+++ Anotações/lacos-de-repeticao.js
@@ -1,16 +1,32 @@
-for (i=10; i<5;i--)
+const utils = require("../bib/novomodulo")
+
+utils.linha(40);
+console.log("TESTE 1: loop for\n"); // UTIL PARA QUANDO SE TEM UM INTERVALO CLARO DEFINIDO
+for (x=10; x>0; x--)
 {
-    console.log(i)
+    console.log( Math.abs(x - 10))
+}
+console.log("break")
+
+utils.linha(40);
+
+console.log("TESTE 2: loopo while\n"); // Util para quando não se tem um intervalo claro e definido
+var i = 0;
+while (true)
+{    
+    console.log("TRUE");
+    if (i >= 9)
+    {
+        console.log(i);
+        console.log("FALSE");
+        console.log("break");
+        break;
+    } else {console.log(i); i++}
 }
 
-var i = 0
-ans = true
-while (ans === true)
-{    
-    console.log("TRUE")
-    if (i == 10)
-    {
-        console.log("break")
-        ans = false
-    } else {print(i); i++}
-}
+utils.linha(40);
+
+var y = 0
+console.log("TESTE 3: loop do while\n"); //Executa uma vez e depois testa
+do {console.log(y); y++} while(y<5);
+console.log(y);
