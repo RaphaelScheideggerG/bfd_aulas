42a66663a5c1c66c301962d2f5e862a45398cb90
Raphael Scheidegger G. <raphaelscheideggerguedes0406@gmail.com>
2025-09-03 20:57:59 -0300

    asd

---
+++ objetos/pessoas/PFDAO.js
@@ -0,0 +1,30 @@
+import { localStorage } from './localStorage.mjs';
+import PF from './PessoaFisica'
+
+export class PFDAO {
+    #pf;
+
+    constructor(pf){
+        if (pf instanceof PF){
+            this.#pf = pf;
+        }
+    }
+
+    toJSON()
+    {
+        // TITULO
+        let vtitle = []
+        
+        // FONES
+        let vfones = []
+        
+        // ENDEREÇO
+
+        return{
+            nome: this.#pf.getNome()
+            email: this.#pf.getEmail()
+            endereco: this.#pf.getEndereco()
+            
+        }
+    }
+} 
