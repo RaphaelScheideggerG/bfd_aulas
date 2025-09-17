072b7ca1cc9f89ead2a5dac90a1797bad5521cba
RaphaelScheideggerG <raphaelscheideggerguedes0406@gmail.com>
2025-09-17 10:40:53 -0300

    atualizado

---
+++ tipovar.js
@@ -0,0 +1,17 @@
+var numero = 5.2; // Tipo Number (1,10, 2.5, 5.3050)
+console.log(numero); // typeof(numero)
+console.log(typeof(numero));
+
+var palavra = "JavaScript"; // Tipo String (Cadeia de Caracteres)
+console.log(palavra);
+console.log(typeof(palavra));
+
+var logica = false; // Tipo Booleano true (Verdadeiro) false (falso)
+console.log(logica);
+console.log(typeof(logica));
+
+var qualquer;
+console.log(qualquer);
+console.log(typeof(qualquer));
+
+// comentário para teste de git 
