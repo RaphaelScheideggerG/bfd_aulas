093fd9912b377d08a9f11e86afa6554a6a7696b8
null <null>
2025-09-22 21:48:22 -0300

    alunoDAO

--- objetos/pessoas/DAOs/AlunoDAO.mjs
+++ objetos/pessoas/DAOs/AlunoDAO.mjs
@@ -2,5 +2,49 @@ import { localStorage } from './localStorage.mjs';
 import {Aluno} from '../Aluno'
 
 export default class AlunoDAO{
+    #aluno
+    constructor(aluno){
+        if (aluno instanceof Aluno){
+            this.#aluno = aluno
+        }
+    }
+
+    toJSON(){
+        // FONES
+        let fones = []
+        for (let fone of this.#aluno.getTelefones()){
+            fones.push({
+                numero: fone.getNumero(),
+                ddd: fone.getDdd(),
+            })
+        }
+
+        return{
+            nome: this.#aluno.getNome(),
+            email: this.#aluno.getEmail(),
+            telefones: fones,
+            endereco: {
+                logradouro: this.#aluno.getEndereco().getLogradouro(),
+                cep: this.#aluno.getEndereco().getCep(),
+            },
+            titulo: {
+                numero: this.#aluno.getTitulo().getNumero(),
+                zona: this.#aluno.getTitulo().getZona(),
+                secao: this.#aluno.getTitulo().getSecao(),
+            },
+            matricula: this.#aluno.getMatricula(),
+            curso: this.#aluno.getCurso(),
+        }
+    }
+    
+    saveJSON() {                                      // 30 // Método para salvar o objeto PJ em formato JSON no localStorage
+        // transforma em string JSON usando stringify() e armazena
+        localStorage.setItem("pf", JSON.stringify(this.toJSON()));
+    }
+    
+    recoveryJSON() {                                  // 34 // Método para recuperar os dados de PJ armazenados
+        // recupera a string armazenada e transforma em JSON usando parse()
+        return JSON.parse(localStorage.getItem("pf"));  // 36
+    }
 
 }
